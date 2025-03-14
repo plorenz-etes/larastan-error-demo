41e3fbab5fafc243c44c6644000a07d6040c3c60
Philipp Lorenz <philipp.lorenz@etes.de>
2025-03-14 15:09:09 +0100

    Initial commit

---
+++ app/Traits/BelongsToTenant.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Tenant;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+trait BelongsToTenant
+{
+    public function tenant(): BelongsTo
+    {
+        return $this->belongsTo(Tenant::class);
+    }
+
+    protected static function bootBelongsToTenant(): void
+    {
+        // here be dragons
+    }
+}
